1c4163a90caa0dd82040528e112b86e952bb97a4
ccx1010sky <ccx1010@gmail.com>
2022-02-20 21:49:18 +0000

    Trumpet constructor and implement playMusic() and calculateMakeup()

--- src/main/java/goods/Trumpet.java
+++ src/main/java/goods/Trumpet.java
@@ -2,6 +2,20 @@ package goods;
 
 import shop.Instrument;
 
-public class Trumpet {
+public class Trumpet extends Instrument  {
     private int numberOfValves;
+
+    public Trumpet(String name, String color, String madeFrom, double sellPrice, double purchasePrice, String classification) {
+        super(name, color, madeFrom, sellPrice, purchasePrice, classification);
+    }
+
+    @Override
+    public String playMusic() {
+        return "didida";
+    }
+
+    @Override
+    public double calculateMakeup() {
+        return 0;
+    }
 }
