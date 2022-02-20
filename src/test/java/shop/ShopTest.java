c408507e04d7047a503d1b39753226b164792a7b
ccx1010sky <ccx1010@gmail.com>
2022-02-20 22:56:03 +0000

    shop canRemoveItem() test passed

--- src/test/java/shop/ShopTest.java
+++ src/test/java/shop/ShopTest.java
@@ -2,6 +2,7 @@ package shop;
 
 import behaviour.ISell;
 import goods.Guitar;
+import goods.Trumpet;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,12 +13,15 @@ import static org.junit.Assert.*;
 public class ShopTest {
     private Shop shop1;
     private Guitar guitar;
+    private Trumpet trumpet;
+
     private ArrayList<ISell> stock;
 
     @Before
     public void before(){
         shop1 = new Shop("shop1");
         guitar = new Guitar("guitar","blue","wood",200,100,"chordophone",6);
+        trumpet = new Trumpet("Trumpet","black","metal",100.00,50.00,"Brass");
         stock = new ArrayList<>();
         }
 
@@ -29,6 +33,13 @@ public class ShopTest {
     @Test
     public void canAddItem(){
         shop1.addItem(guitar);
+        assertEquals(1,shop1.getStockCount());
+    }
+    @Test
+    public void canRemoveItem(){
+        shop1.addItem(guitar);
+        shop1.addItem(trumpet);
+        shop1.removeItem(guitar);
         assertEquals(1,shop1.getStockCount());
     }
 
