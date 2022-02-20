8a5e0c8bd0734ca7caf179050f7f610075059f4b
ccx1010sky <ccx1010@gmail.com>
2022-02-20 23:19:59 +0000

    update drum with numOfDrumSticks property

--- src/main/java/goods/Drum.java
+++ src/main/java/goods/Drum.java
@@ -3,9 +3,11 @@ package goods;
 import shop.Instrument;
 
 public class Drum extends Instrument {
+    private int numOfDrumSticks;
 
-    public Drum(String name, String color, String madeFrom, double sellPrice, double purchasePrice, String classification) {
+    public Drum(String name, String color, String madeFrom, double sellPrice, double purchasePrice, String classification, int numOfDrumSticks) {
         super(name, color, madeFrom, sellPrice, purchasePrice, classification);
+        this.numOfDrumSticks = numOfDrumSticks;
     }
 
     @Override
--- src/main/java/goods/Drumstick.java
+++ src/main/java/goods/Drumstick.java
@@ -6,6 +6,7 @@ import shop.Instrument;
 public class Drumstick extends Accessory{
 
 
+
     public Drumstick(String name, String color, String madeFrom, double sellPrice, double purchasePrice, Instrument instrumentType) {
         super(name, color, madeFrom, sellPrice, purchasePrice, instrumentType);
     }
