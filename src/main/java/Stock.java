4fb3d1902d9bf06f5777f2dc51e13b124678be2e
ccx1010sky <ccx1010@gmail.com>
2022-02-20 20:12:15 +0000

    create abstract class Stock and constructor

---
+++ src/main/java/Stock.java
@@ -0,0 +1,15 @@
+public abstract class Stock {
+    private String name;
+    private String color;
+    private String madeFrom;
+    private double sellPrice;
+    private double purchasePrice;
+
+    public Stock(String name, String color, String madeFrom, double sellPrice, double purchasePrice) {
+        this.name = name;
+        this.color = color;
+        this.madeFrom = madeFrom;
+        this.sellPrice = sellPrice;
+        this.purchasePrice = purchasePrice;
+    }
+}
